fde3c67d76727cb6d04215856195e7d320c2d80a
SD18 <SD18@mitt.ca>
2018-09-07 21:37:46 -0500

    another commit

--- ExerciseOne/Scripts/jsexercise.js
+++ ExerciseOne/Scripts/jsexercise.js
@@ -9,12 +9,13 @@
     var average = (num1 + num2 + num3 + num4 + num5) / 2;
     var largest = (Math.max(num1, num2, num3, num4, num5));
     var smallest = (Math.min(num1, num2, num3, num4, num5))
-    $("#sum").text("The sum is" + sum);
-    $("#product").text("The product is " + product);
-    $("#average").text("The average is " + average);
-    $("#largest").text("The largest is " + largest);
-    $("#smallest").text("The smallest is " + smallest);
-})
+    if ($("#input1").val() && $("#input2").val() && $("#input3").val() && $("#input4").val() && $("#input5").val() !== "") {
+        $("#sum").text("The sum is" + sum);
+        $("#product").text("The product is " + product);
+        $("#average").text("The average is " + average);
+        $("#largest").text("The largest is " + largest);
+        $("#smallest").text("The smallest is " + smallest);
+    }})
 $("#codeMath").hide();
 $("#showMathCode").click(function () {
     $("#codeMath").toggle();
@@ -24,6 +25,7 @@ $("#btn1").click(function () {
     var number1 = Number($("#number1").val());
     var number2 = Number($("#number2").val());
     var numbers = [];
+    
     for (var loop = 1; loop <= 100; loop++) {
         if (loop % number1 == 0 && loop % number2 == 0) {
             numbers.push("FIZZBUZZ")
@@ -35,8 +37,12 @@ $("#btn1").click(function () {
             numbers.push(loop);
         }
     }
-    var output = numbers.join(' , ');
-    $("#fizzbuzz").text(output);
+    
+   
+    if ($("#number1").val() && $("#number2").val() !== "") {
+        var result = numbers.join(' , ');
+        $("#fizzbuzz").text(result);
+    }
 })
 $("#codeFZ").hide();
 $("#showFZ").click(function () {
@@ -48,8 +54,11 @@ $("#btnfact").click(function () {
     for (var i = num; i >= 1; i--) {
         fact = fact * i;
     }
-    var result = fact;
-    $("#factorial").text("The factorial is " + result);
+    
+    if ($("#num").val() !=="") {
+        var result = fact;
+        $("#factorial").text("The factorial is " + result);
+    }
 })
 $("#codeFT").hide();
 $("#showFac").click(function () {
@@ -58,11 +67,16 @@ $("#showFac").click(function () {
 $("#btn3").click(function () {
     var someText = $("#sometext").val();
     var checkForPalindrome = someText.split('').reverse().join('');
-    if (someText === checkForPalindrome) {
+    if ($("#sometext").val("")) {
+        $("#palindrome").text("");
+
+}
+    if (someText === checkForPalindrome && someText !=="") {
         $("#palindrome").text("The word is a palindrome!")
-    } else {
+    } else if(someText !=="" && someText !== checkForPalindrome){
         $("#palindrome").text("The word is not a palindrome!")
     }
+    $("#sometext").val("")
 })
 $("#codePD").hide();
 $("#showPali").click(function () {
